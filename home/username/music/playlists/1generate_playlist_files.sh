f7556e02487842c3df78f2e0edea91ec2a96172f
Ramon Fischer <Ramon_Fischer@hotmail.de>
2020-07-23 14:04:20 +0200

    add shell script to generate 'm3u8' playlist files

---
+++ home/username/music/playlists/1generate_playlist_files.sh
@@ -0,0 +1,94 @@
+#!/bin/bash
+command_list=(cat find rm sort)
+checkCommands()
+{
+    for current_command in "${command_list[@]}"
+    do
+        unalias ${current_command} 2>/dev/null
+        if [[ ! $(command -v ${current_command} 2>/dev/null) ]]
+        then
+            /bin/echo -e "\e[01;31mCould not find command '${current_command}'.\e[0m"
+            exit 1
+        fi
+    done
+}
+
+checkCommands
+
+# define global directories
+IFS=$'\n'
+declare -a music_directory_list
+declare -a existing_playlist_filename_list
+declare -a new_playlist_filename_list
+music_directory_list=("../normal_music" "../unusual_music")
+playlist_filename_suffix="m3u8"
+playlist_all_filename="all.${playlist_filename_suffix}"
+existing_playlist_filename_list=($(/usr/bin/find . -type f -name "*.${playlist_filename_suffix}"))
+
+checkAndPromptExistingPlaylists()
+{
+    if [[ "${existing_playlist_filename_list[@]}" != "" ]]
+    then
+        read -p $'\e[01;31mFound existing playlists in this directory. Remove them before generating new playlists? (Y/n): \e[0m' remove_playlist_files
+
+        case "${remove_playlist_files:-y}" in
+            "y"|"Y")
+                /bin/rm --force --verbose "${existing_playlist_filename_list[@]}"
+                ;;
+
+            "n"|"N")
+                continue
+        esac
+    fi
+}
+
+concatenateGeneratedPlaylistFiles()
+{
+    local music_directory
+    local playlist_filename_list=()
+
+    for music_directory in ${music_directory_list[@]}
+    do
+        playlist_filename_list+=("${music_directory##*/}.${playlist_filename_suffix}")
+    done
+
+    /bin/cat "${playlist_filename_list[@]}" > "${playlist_all_filename}"
+
+    unset music_directory
+    unset playlist_filename_list
+}
+
+#convertUnixNewlines()
+#{
+#    local new_playlist_filename_list=($(/usr/bin/find . -type f -name "*.${playlist_filename_suffix}"))
+#
+#    if [[ "${new_playlist_filename_list[@]}" != "" ]]
+#    then
+#        /usr/bin/unix2dos "${new_playlist_filename_list[@]}"
+#    fi
+#}
+
+generatePlaylistFiles()
+{
+    local music_directory
+    local playlist_filename
+
+    for music_directory in ${music_directory_list[@]}
+    do
+        playlist_filename="${music_directory##*/}.${playlist_filename_suffix}"
+        /usr/bin/find "${music_directory}" -type f | /usr/bin/sort --ignore-case > "${playlist_filename}"
+    done
+    unset music_directory
+    unset playlist_filename
+}
+
+main()
+{
+    checkAndPromptExistingPlaylists
+
+    generatePlaylistFiles
+
+    concatenateGeneratedPlaylistFiles
+}
+
+main
