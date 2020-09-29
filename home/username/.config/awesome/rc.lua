93e22e50fa6f3fd2fb67fdacdf9c877b87f830ed
Ramon Fischer <Ramon_Fischer@hotmail.de>
2020-09-29 12:32:42 +0200

    rename shell script

--- home/username/.config/awesome/rc.lua
+++ home/username/.config/awesome/rc.lua
@@ -131,7 +131,7 @@ email = "thunderbird-bin"
 downloader = "jdownloader"
 scanner = "xsane"
 office = "libreoffice"
-steam = "start_steam"
+steam = "steam"
 mymainmenu = awful.menu({ items = { { "awesome", myawesomemenu, beautiful.awesome_icon },
                                     { "open terminal", terminal },
                                     -- custom - 20200510 - rfischer: add browser variable
