fb7343b42d111e2bf33eae7ce031cbc94b847d73
Ramon Fischer <Ramon_Fischer@hotmail.de>
2021-01-11 18:52:14 +0100

    check for broken dependencies

--- root/bin/update.sh
+++ root/bin/update.sh
@@ -44,6 +44,7 @@ script_name="${0##*/}"
 /bin/echo -e "\e[01;33mChecking for new updates...\e[0m"
 /usr/bin/sudo --shell --user="${SUDO_USER}" /home/${SUDO_USER}/bin/gem update
 /usr/bin/sudo --shell --user="${SUDO_USER}" /home/${SUDO_USER}/bin/pip-review --auto --user --no-warn-script-location
+/usr/bin/sudo --shell --user="${SUDO_USER}" /home/${SUDO_USER}/bin/pip check
 /usr/bin/flatpak update
 /usr/bin/eix-sync
 /usr/bin/emerge --ask --update --deep --newuse --tree --verbose @world
