c99a8700599e4df6c116a1a0c0c66bbed613c3d4
Ramon Fischer <Ramon_Fischer@hotmail.de>
2022-10-15 23:31:15 +0200

    remove loops, rename in parallel

--- home/username/music/correct_filenames_and_directory_names.sh
+++ home/username/music/correct_filenames_and_directory_names.sh
@@ -17,22 +17,23 @@
 
 # correct filenames and directory names in order to be conform with the fat naming convention.
 # the slash character is left out, since linux does not allow this character in filenames.
-declare -a fat_illegal_character_array
-fat_illegal_character_array=("<" ">" ":" "\"" "\\" "?" "*")
+# adding the character anyways, will match everything.
+fat_illegal_character_list='<>:"\?*'
 declare -a music_directory_array
 music_directory_array=("audiobooks" "normal_music" "playlists" "podcasts" "record" "unusual_music")
+available_processors=$(/usr/bin/nproc --all --ignore="1")
+xargs_max_args="1"
 
-for music_directory in "${music_directory_array[@]}"
-do
-    for fat_illegal_character in "${fat_illegal_character_array[@]}"
-    do
-        /usr/bin/find "${music_directory}" \
-            -regextype posix-extended \
-            \( \
-                -type f -iregex ".*${fat_illegal_character}.*" \
-                -or \
-                -type d -iregex ".*${fat_illegal_character}.*" \
-            \) \
-            -exec /usr/bin/rename --verbose "${fat_illegal_character}" "_" "{}" +
-    done
-done
+/usr/bin/find "${music_directory_array[@]}" \
+    -regextype posix-extended \
+    \( \
+        -type f -iregex ".*[${fat_illegal_character_list}].*" \
+        -or \
+        -type d -iregex ".*[${fat_illegal_character_list}].*" \
+    \) \
+    -print0 \
+        | /usr/bin/xargs \
+            --null \
+            --max-procs="${available_processors}" \
+            --max-args="${xargs_max_args}" \
+            /usr/bin/perl-rename --verbose "s/[${fat_illegal_character_list}]/_/g"
