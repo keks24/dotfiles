24491bc394c558ad8f859288aadec998943f98fe
Ramon Fischer <Ramon_Fischer@hotmail.de>
2022-10-29 21:47:15 +0200

    improve readability

--- home/username/music/playlists/update_domian_live_2021_fanclub.sh
+++ home/username/music/playlists/update_domian_live_2021_fanclub.sh
@@ -18,7 +18,7 @@
 home_directory="${HOME}"
 youtube_channel_name="domian_live_2021_fanclub"
 youtube_channel_url="https://www.youtube.com/user/danielskate14"
-youtube_video_url="https://www.youtube.com/watch?v="
+youtube_url="https://www.youtube.com"
 youtube_playlist_file="${home_directory}/music/playlists/youtube_-_${youtube_channel_name}.m3u8"
 download_archive_list_file="${home_directory}/music/youtube-dl/archive_lists/youtube_-_${youtube_channel_name}.list"
 
@@ -27,5 +27,5 @@ download_archive_list_file="${home_directory}/music/youtube-dl/archive_lists/you
     --download-archive="${download_archive_list_file}" \
     --force-download-archive \
     --get-id "${youtube_channel_url}" \
-        | /usr/bin/xargs --replace="{}" echo "${youtube_video_url}{}" \
+        | /usr/bin/xargs --replace="{}" echo "${youtube_video_url}/watch?v={}" \
         | /usr/bin/tee --append "${youtube_playlist_file}"
