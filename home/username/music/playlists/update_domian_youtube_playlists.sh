9b9d0fe06d78d52ba1eb6d2749228e110578a58f
Ramon Fischer <Ramon_Fischer@hotmail.de>
2022-11-02 01:22:21 +0100

    get youtube video ids from multiple channels, correct typo, rename script filename

--- home/username/music/playlists/update_domian_youtube_playlists.sh
+++ home/username/music/playlists/update_domian_youtube_playlists.sh
@@ -17,15 +17,26 @@
 
 home_directory="${HOME}"
 youtube_url="https://www.youtube.com"
-youtube_channel_name="domian_live_2021_fanclub"
-youtube_channel_url="${youtube_url}/user/danielskate14"
-youtube_playlist_file="${home_directory}/music/playlists/youtube_-_${youtube_channel_name}.m3u8"
-download_archive_list_file="${home_directory}/music/youtube-dl/archive_lists/youtube_-_${youtube_channel_name}.list"
+declare -A youtube_channel_array
+youtube_channel_array=( \
+                        ["domian_live_2021_fanclub"]="${youtube_url}/user/danielskate14" \
+                        ["domian4ever"]="${youtube_url}/channel/UCS3EcW5FS_p0MzapOPcNooA" \
+                        ["domianarchiv_de"]="${youtube_url}/c/DomianarchivDe" \
+                      )
+music_directory_path="${home_directory}/music"
+playlist_directory_path="${music_directory_path}/playlists"
+youtube_download_archive_list_directory_path="${music_directory_path}/youtube-dl/archive_lists"
 
-/usr/bin/yt-dlp \
-    --ignore-config \
-    --download-archive="${download_archive_list_file}" \
-    --force-download-archive \
-    --get-id "${youtube_channel_url}" \
-        | /usr/bin/xargs --replace="{}" echo "${youtube_video_url}/watch?v={}" \
-        | /usr/bin/tee --append "${youtube_playlist_file}"
+for youtube_channel in "${!youtube_channel_array[@]}"
+do
+    youtube_playlist_file="${playlist_directory_path}/youtube_-_${youtube_channel}.m3u8"
+    youtube_download_archive_list_file="${youtube_download_archive_list_directory_path}/youtube_-_${youtube_channel}.list"
+
+    /usr/bin/yt-dlp \
+        --ignore-config \
+        --download-archive="${youtube_download_archive_list_file}" \
+        --force-download-archive \
+        --get-id "${youtube_channel_array[${youtube_channel}]}" \
+            | /usr/bin/xargs --replace="{}" echo "${youtube_url}/watch?v={}" \
+            | /usr/bin/tee --append "${youtube_playlist_file}"
+done
