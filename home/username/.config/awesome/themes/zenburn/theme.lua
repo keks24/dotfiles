aab26c844dad3e32f06ab683f0dc639d65923828
Ramon Fischer <Ramon_Fischer@hotmail.de>
2024-09-02 13:25:14 +0200

    add license boiler plate, use 'dpi()' function for 'useless_gap'

--- home/username/.config/awesome/themes/zenburn/theme.lua
+++ home/username/.config/awesome/themes/zenburn/theme.lua
@@ -1,7 +1,27 @@
-local themes_path = require("gears.filesystem").get_themes_dir()
-local theme = dofile(themes_path .. "zenburn/theme.lua")
+--[[
+#############################################################################
+# Copyright 2024 Ramon Fischer                                              #
+#                                                                           #
+# Licensed under the Apache License, Version 2.0 (the "License");           #
+# you may not use this file except in compliance with the License.          #
+# You may obtain a copy of the License at                                   #
+#                                                                           #
+#     http://www.apache.org/licenses/LICENSE-2.0                            #
+#                                                                           #
+# Unless required by applicable law or agreed to in writing, software       #
+# distributed under the License is distributed on an "AS IS" BASIS,         #
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  #
+# See the License for the specific language governing permissions and       #
+# limitations under the License.                                            #
+#############################################################################
+--]]
+
+local dpi         = require("beautiful.xresources").apply_dpi
+local gears       = require("gears.filesystem")
+local themes_path = gears.get_themes_dir()
+local theme       = dofile(themes_path .. "zenburn/theme.lua")
 
     theme.font = "terminus 8"
-    theme.useless_gap = "5"
+    theme.useless_gap = "dpi(5)"
 
 return theme
