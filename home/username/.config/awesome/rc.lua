ce022481a812852986f6a1a481826e0c1eba1873
Ramon Fischer <Ramon_Fischer@hotmail.de>
2024-12-06 14:44:52 +0100

    manipulate window layout with arrow keys

--- home/username/.config/awesome/rc.lua
+++ home/username/.config/awesome/rc.lua
@@ -320,10 +320,13 @@ root.buttons(gears.table.join(
 globalkeys = gears.table.join(
     awful.key({ modkey,           }, "s",      hotkeys_popup.show_help,
               {description="show help", group="awesome"}),
+    -- custom - 20241206T144012+0100 - rfischer: manipulate window layout with arrow keys.
+    --[[
     awful.key({ modkey,           }, "Left",   awful.tag.viewprev,
               {description = "view previous", group = "tag"}),
     awful.key({ modkey,           }, "Right",  awful.tag.viewnext,
               {description = "view next", group = "tag"}),
+    --]]
     awful.key({ modkey,           }, "Escape", awful.tag.history.restore,
               {description = "go back", group = "tag"}),
 
@@ -370,10 +373,22 @@ globalkeys = gears.table.join(
     awful.key({ modkey, "Shift"   }, "q", awesome.quit,
               {description = "quit awesome", group = "awesome"}),
 
+    --[[
     awful.key({ modkey,           }, "l",     function () awful.tag.incmwfact( 0.05)          end,
               {description = "increase master width factor", group = "layout"}),
     awful.key({ modkey,           }, "h",     function () awful.tag.incmwfact(-0.05)          end,
               {description = "decrease master width factor", group = "layout"}),
+    --]]
+    -- custom - 20241206T143148+0100 - rfischer: manipulate window layout with arrow keys.
+    awful.key({ modkey,           }, "Up",    function () awful.client.incwfact(-0.05)        end,
+              {description = "decrease master height factor", group = "layout"}),
+    awful.key({ modkey,           }, "Down",  function () awful.client.incwfact( 0.05)        end,
+              {description = "increase master height factor", group = "layout"}),
+    awful.key({ modkey,           }, "Left",  function () awful.tag.incmwfact(-0.05)          end,
+              {description = "decrease master width factor", group = "layout"}),
+    awful.key({ modkey,           }, "Right", function () awful.tag.incmwfact( 0.05)          end,
+              {description = "increase master width factor", group = "layout"}),
+
     awful.key({ modkey, "Shift"   }, "h",     function () awful.tag.incnmaster( 1, nil, true) end,
               {description = "increase the number of master clients", group = "layout"}),
     awful.key({ modkey, "Shift"   }, "l",     function () awful.tag.incnmaster(-1, nil, true) end,
