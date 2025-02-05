353193456e514d3528481ddbbab4895e2f97d9cb
Ramon Fischer <Ramon_Fischer@hotmail.de>
2025-02-05 11:43:56 +0100

    only move instance 'mail' and focus screen, move instance 'calendar' without focussing

--- home/username/.config/awesome/rc.lua
+++ home/username/.config/awesome/rc.lua
@@ -615,9 +615,12 @@ awful.rules.rules = {
       properties = { screen = 2, tag = "web", switchtotag = true,
       function() awful.screen.focus(screen[2]) end } },
     -- custom - 20241106T140033+0100 - rfischer: always open "thunderbird" on "screen 2", tag "e-mail" and focus "screen 2".
-    { rule = { class = "thunderbird-esr" },
+    { rule = { instance = "Mail", class = "thunderbird-esr" },
       properties = { screen = 2, tag = "e-mail", switchtotag = true,
       function() awful.screen.focus(screen[2]) end } },
+    -- always open "calendar" windows on "screen 2"
+    { rule = { instance = "Calendar", class = "thunderbird-esr" },
+      properties = { screen = 2, tag = "e-mail" } },
     -- custom - 20241108T081820+0100 - rfischer: always open "libreoffice" on "screen 2", tag "office" and focus "screen 2".
     { rule_any = { class = {
                     "Soffice",
