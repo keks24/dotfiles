a395c83126489d73c59b7a0030f6916e281d7f8f
Ramon Fischer <Ramon_Fischer@hotmail.de>
2025-07-08 15:26:27 +0200

    save environment variable, improve wording, improve readability, backslash is not needed in 'grep'

--- root/.local/bin/update.sh
+++ root/.local/bin/update.sh
@@ -51,24 +51,52 @@ checkCommands
 # define global variables
 script_directory_path="${0%/*}"
 script_name="${0##*/}"
+sudo_user="${SUDO_USER}"
 no_tmpfs_file="/etc/portage/package.env/no_tmpfs.conf"
 if [[ ! -f "${no_tmpfs_file}" ]]
 then
-    echo -e "\e[01;31mCould not find '${no_tmpfs_file}'.\e[0m" >&2
+    echo -e "\e[01;31mCould not find file '${no_tmpfs_file}' or is not a file.\e[0m" >&2
     exit 1
 else
-    large_package_list=$(/bin/grep --extended-regexp --only-matching "[0-9a-zA-Z]+-[0-9a-zA-Z]+\/[-0-9a-zA-Z]+" "${no_tmpfs_file}")
+    large_package_list=$(
+                            /bin/grep \
+                                --extended-regexp \
+                                --only-matching \
+                                "[0-9a-zA-Z]+-[0-9a-zA-Z]+/[-0-9a-zA-Z]+" \
+                                "${no_tmpfs_file}"
+                        )
 fi
 
 /usr/bin/clear
 /usr/bin/logger --tag "update" --id --stderr "${script_directory_path}/${script_name}: executed"
 
 echo -e "\e[01;33mChecking for new updates...\e[0m" >&2
-#/usr/bin/sudo --shell --user="${SUDO_USER}" CCACHE_DISABLE="1" /home/${SUDO_USER}/bin/gem update
-/usr/bin/sudo --shell --user="${SUDO_USER}" /home/${SUDO_USER}/bin/pip-review --auto --user --no-warn-script-location
-/usr/bin/sudo --shell --user="${SUDO_USER}" /home/${SUDO_USER}/bin/pip check
-#/usr/bin/sudo --shell --user="${SUDO_USER}" /usr/bin/flatpak update
-#/bin/su - "${SUDO_USER}" --command="/usr/bin/npm update -g"
+#/usr/bin/sudo \
+#    --shell \
+#    --user="${sudo_user}" \
+#    CCACHE_DISABLE="1" \
+#    /home/${sudo_user}/bin/gem update
+/usr/bin/sudo \
+    --shell \
+    --user="${sudo_user}" \
+    /home/${sudo_user}/bin/pip-review \
+        --auto \
+        --user \
+        --no-warn-script-location
+/usr/bin/sudo \
+    --shell \
+    --user="${sudo_user}" \
+    /home/${sudo_user}/bin/pip \
+        check
+#/usr/bin/sudo \
+#    --shell \
+#    --user="${sudo_user}" \
+#    /usr/bin/flatpak \
+#        update
+#/bin/su \
+#    - \
+#    "${sudo_user}" \
+#    --command="/usr/bin/npm update -g"
 /usr/bin/eix-sync
 /usr/bin/eselect news read
 echo -e "\n\e[01;33mPress any key to continue...\e[0m" >&2
@@ -78,11 +106,28 @@ then
     echo -e "\e[01;31mA new version of 'sys-apps/portage' was found. Updating it first...\e[0m" >&2
     /usr/bin/emerge --ask --oneshot sys-apps/portage
 fi
-/usr/bin/emerge --ask --update --deep --changed-use --tree --verbose --exclude="${large_package_list//$'\n'/ }" @world
-# always compile large packages as last packages
-/usr/bin/emerge --update --deep --changed-use --tree --verbose @world
+/usr/bin/emerge \
+    --ask \
+    --update \
+    --deep \
+    --changed-use \
+    --tree \
+    --verbose \
+    --exclude="${large_package_list//$'\n'/ }" \
+    @world
+# always compile large packages last
+/usr/bin/emerge \
+    --update \
+    --deep \
+    --changed-use \
+    --tree \
+    --verbose \
+    @world
 /usr/sbin/etc-update
-/usr/bin/emerge --ask --depclean --verbose
+/usr/bin/emerge \
+    --ask \
+    --depclean \
+    --verbose
 /usr/bin/glsa-check --test all
 /usr/bin/glsa-check --list
 /usr/bin/glsa-check --fix --quiet all
