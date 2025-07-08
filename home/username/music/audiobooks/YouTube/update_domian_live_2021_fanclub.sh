f2fceac75f43b0bf3bb4230d3cc5faab218a1b79
Ramon Fischer <Ramon_Fischer@hotmail.de>
2025-07-08 12:18:38 +0200

    improve readability

--- home/username/music/audiobooks/YouTube/update_domian_live_2021_fanclub.sh
+++ home/username/music/audiobooks/YouTube/update_domian_live_2021_fanclub.sh
@@ -20,5 +20,7 @@ then
     echo -e "\e[01;31mCould not find directory: './Domain Live 2021 FanClub' or is not a directory.\e[0m" >&2
     exit 1
 else
-    /usr/bin/youtube-dl --ignore-config --config-location="/home/ramon/.config/youtube-dl/config_domian"
+    /usr/bin/youtube-dl \
+        --ignore-config \
+        --config-location="/home/ramon/.config/youtube-dl/config_domian"
 fi
